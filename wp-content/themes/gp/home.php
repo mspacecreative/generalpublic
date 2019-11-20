92c717e94282fe922cca811814a288b132731fa5
Matt <matt@mspacecreative.com>
2019-11-20 15:13:27 -0400

    c

--- wp-content/themes/gp/home.php
+++ wp-content/themes/gp/home.php
@@ -44,7 +44,7 @@ Template Name: Home Page
 
 <div id="logo">
 
-	<img src="<?php bloginfo('template_url'); ?>/images/branding/general-public-partners-white.svg" />
+	<img src="<?php bloginfo('template_url'); ?>/images/branding/general-public-partners-blue.svg" />
 
 </div>
 
